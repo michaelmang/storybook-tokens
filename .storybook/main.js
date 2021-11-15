e52a0816bbbf18ebb2c1701622da2003b04c86f4
Mike Mangialardi <mikemangialardi94@gmail.com>
2021-11-15 17:51:08 -0500

    Install css; Resolve error

--- .storybook/main.js
+++ .storybook/main.js
@@ -15,4 +15,17 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.node = {
+      fs: 'empty',
+    };
+
+    // Return the altered config
+    return config;
+  },
 };
