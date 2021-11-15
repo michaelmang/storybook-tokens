b47d9d148088718ce927e24d8bc43baba364a481
Mike Mangialardi <mikemangialardi94@gmail.com>
2021-11-15 17:44:58 -0500

    Resolve postcss error

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,16 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["../preset.js", "@storybook/addon-essentials"],
+  addons: [
+    "../preset.js",
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
 };
